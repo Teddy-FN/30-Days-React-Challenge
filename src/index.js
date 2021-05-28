950a472a1dbfbd8700e2c02b98d04ebff2a0744d
Teddy Ferdian <tedd.ferdy@gmail.com>
2021-05-28 08:18:22 +0700

    React Challenge Day 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style/props.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Teddy from './img/teddy.png'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// Header 
+const Header = ({data, navs}) => {
+  console.table('INI PROPS DATA',data)
+  console.log('INI PROPS NAVS', navs)
+  const navigasi = navs.map((items, idx) => {
+    return <ul key={idx}><li>{items}</li></ul>
+  })
+  return (
+    <div className="card-wrapper">
+        <header className="header-wrapper">
+          <section className="logo-wrapper">
+            <h1>User Profile</h1>
+          </section>
+          <nav>
+            {navigasi}
+          </nav>
+        </header>
+        <main>
+          <article className="profile-user-wrapper"> 
+            <section className="profile-pic-wrapper">
+              <img src={Teddy} alt="profile-user"/>
+            </section>
+            <section>
+              <h1>{data.name.firstName} {data.name.lastName}</h1>
+            </section>
+          </article>
+        </main>
+        <footer>
+
+        </footer>
+    </div>
+  )
+}
+
+const App = () => {
+  const data = {
+    name: {
+      firstName: 'Teddy',
+      lastName: 'Ferdian'
+    },
+    age: 22,
+    address: {
+      street: 'Mojo',
+      city: 'Sukoharjo'
+    },
+    job: 'Software Engginer',
+    jobSkill: ['Frontend', 'Backend', 'FullStack'],
+    techWebs : 'HTML, CSS, JavaScript, React, Node, GIT, Express, MongoDB',
+    date: new Date()
+  }
+  const navs = [
+    'Contact', 'About', 'Profile'
+  ]
+  return(
+    <div>
+        <Header data={data} navs={navs}/>
+    </div>
+  )
+}
+const rootElements = document.getElementById('root')
+ReactDOM.render(<App />, rootElements)
